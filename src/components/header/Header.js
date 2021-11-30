620f81e99073f17ba14379f6f108fa07e1fdce68
caio1020ee <93018302+caio1020ee@users.noreply.github.com>
2021-11-30 19:51:46 -0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -70,3 +70,61 @@ const superheroapi = {
 
 
 export default ()=> superheroapi;
+
+
+background-color: #48ABC2;
+
+    header{
+        height: 100px;
+        max-width: 1000px;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .leftSide {
+            height: 70px;
+
+            &:hover img{
+                height: 75px;
+                transform: translateY(-3px);
+            }
+
+            a, img{
+                height: inherit;
+            }
+        }
+        .rightSide{
+            font-size: 20px;
+            color: #F4EC74;
+            font-weight: 700;
+            text-transform: uppercase;
+
+            span{
+                text-shadow: 1px 1px 1px black;
+            }
+        }
+    }
+
+@media (max-width: 768px){
+    padding: 0px 20px;
+}
+
+@media (max-width: 375px){
+    .leftSide{
+        height: 100%;
+        display: flex;
+        align-items: center;
+
+        a, img{
+            height: 50px !important;
+        }
+    }
+    .rightSide{
+        font-size: 17px !important;
+        span{
+
+        }
+    }
+}
+`;
